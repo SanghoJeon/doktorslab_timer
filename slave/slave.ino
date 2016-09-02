800792979cf87bd0d53fa3bea56f490584a592e4
Ray Suhyun Lee <raysuhyunlee@gmail.com>
2016-09-03 01:22:10 +0900

    Slave code complete

--- slave/slave.ino
+++ slave/slave.ino
@@ -1,27 +1,48 @@
 #include  <SPI.h>
 #include "RF24.h"
 
-int msg[1];
-RF24 radio(7,8);
-#define CHANNEL 2
-#define PIPE 1
-const uint8_t pipe[][6] = {"1Node", "2Node"};
-int SW1 = 7;
+/* radio */
+#define PIPE_NUM 6  // there are 6 pipes in one channel
+int msg[2];
+RF24 radio(9,10);
+#define CHANNEL 1   // Change this to set rf channel
+#define PIPE 1      // Change this to set rf pipe
+const uint8_t pipe[PIPE_NUM][6] = {"1Node", "2Node", "3Node", "4Node", "5Node", "6Node"};
+
+/* switches */
+#define SWITCH_NUM 3
+const char sw[SWITCH_NUM] = {2, 3, 4};
+char isPressed[SWITCH_NUM];
+
 
 void setup(void) {
-  //Serial.begin(9600);
+  Serial.begin(9600);
   radio.begin();
   radio.powerUp();
-  //Serial.println(radio.getDataRate(), DEC);
   radio.setChannel(CHANNEL);
   radio.openWritingPipe(pipe[PIPE]);
+
+  for(int i=0; i<SWITCH_NUM; i++) {
+    pinMode(sw[i], INPUT_PULLUP);
+  }
 }
 
 void loop(void) {
-  //if (digitalRead(SW1) == HIGH){
-  msg[0] = 111;
-  radio.write(msg, sizeof(int));
-  
-  delay(1000);
-  //}
+  byte buf;
+  for(int i=0; i<SWITCH_NUM; i++) {
+    buf = digitalRead(sw[i]);
+    
+    if (buf == LOW && isPressed[i] == 0) {
+      msg[0] = 'b';
+      msg[1] = '0' + i;
+      radio.write(msg, sizeof(msg));
+      Serial.print(msg[0]);
+      Serial.println(msg[1]);
+      isPressed[i] = 1;
+      delay(50);  // to prevent chattering
+    } else if (buf == HIGH && isPressed[i] == 1) {
+      isPressed[i] = 0;
+      delay(50);  // to prevent chattering
+    }
+  }
 }
